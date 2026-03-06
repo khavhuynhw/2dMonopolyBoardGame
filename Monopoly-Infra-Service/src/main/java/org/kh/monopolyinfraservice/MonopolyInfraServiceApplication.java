9211540b12e520d6e962d96eb4b78b9a9525a662
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2026-03-06 19:46:31 +0700

    p

--- Monopoly-Infra-Service/src/main/java/org/kh/monopolyinfraservice/MonopolyInfraServiceApplication.java
+++ Monopoly-Infra-Service/src/main/java/org/kh/monopolyinfraservice/MonopolyInfraServiceApplication.java
@@ -2,8 +2,10 @@ package org.kh.monopolyinfraservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MonopolyInfraServiceApplication {
 
     public static void main(String[] args) {
